d1486a97e43db06071b5f81b68020e1c034fdc36
RJ Trujillo <eyecantcu@pm.me>
2023-06-13 11:13:56 -0600

    feat: Add script to add JetBrains IDEs to existing distrobox install
    These are way too big to ship in the base image unfortunately

---
+++ jb-additions.sh
@@ -0,0 +1,14 @@
+distrobox-enter -n dev-arch -- '  paru -S aur/android-studio --noconfirm'
+distrobox-enter -n dev-arch -- '  distrobox-export --app android-studio'
+
+distrobox-enter -n dev-arch -- '  paru -S aur/clion --noconfirm'
+distrobox-enter -n dev-arch -- '  distrobox-export --app clion'
+
+distrobox-enter -n dev-arch -- '  paru -S aur/intellij-idea-ultimate-edition --noconfirm'
+distrobox-enter -n dev-arch -- '  paru -S aur/intellij-idea-ultimate-edition-jre --noconfirm'
+distrobox-enter -n dev-arch -- '  distrobox-export --app intellij-idea-ultimate-edition'
+
+distrobox-enter -n dev-arch -- '  paru -S aur/pycharm-professional --noconfirm'
+distrobox-enter -n dev-arch -- '  distrobox-export --app pycharm-professional'
+
+distrobox-enter -n dev-arch -- '  yes | paru -Sccd'
